1829d2490a0778551c47d7523e198dea57171003
Georgii <skeanparty@MacBook-Air-Georgy.local>
2025-05-21 00:32:10 +0200

    Update layout and images

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "19827579"
+    serviceWorkerVersion: "3612005518"
   }
 });
